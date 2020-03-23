eb73fb072748fc284ba8138d3f0a8b58c73413b8
dashuredis <13917689657@163.com>
2020-03-23 12:58:28 +0800

    sdfsdfsdf

--- examples/mycachedapp/mycachedapp.go
+++ examples/mycachedapp/mycachedapp.go
@@ -28,6 +28,7 @@ func main() {
 		fmt.Println("Found value in cache:", res.Data().(*myStruct).text)
 		fmt.Println("11111111111111111")
 		fmt.Println("2222222")
+		fmt.Println("2222dddddddd222")
 	} else {
 		fmt.Println("Error retrieving value from cache:", err)
 	}
