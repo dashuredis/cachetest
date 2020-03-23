029247dcc1fe0ba16a81592db910806ace948a25
dashuredis <13917689657@163.com>
2020-03-23 11:22:44 +0800

    xiugai

--- examples/mycachedapp/mycachedapp.go
+++ examples/mycachedapp/mycachedapp.go
@@ -26,6 +26,7 @@ func main() {
 	res, err := cache.Value("someKey")
 	if err == nil {
 		fmt.Println("Found value in cache:", res.Data().(*myStruct).text)
+		fmt.Println("11111111111111111")
 	} else {
 		fmt.Println("Error retrieving value from cache:", err)
 	}
